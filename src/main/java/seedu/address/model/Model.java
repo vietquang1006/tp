0d192e54e916bf38926d45c1cfb6f98aa6ada79f
henghengyh <yihenglee6@gmail.com>
2026-03-12 09:49:03 +0800

    Add getSortedFilteredPersonList in Model.java

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -80,6 +80,9 @@ public interface Model {
     /** Returns an unmodifiable view of the filtered person list */
     ObservableList<Person> getFilteredPersonList();
 
+    /** Returns an unmodifiable view of the filtered person list */
+    ObservableList<Person> getSortedFilteredPersonList();
+
     /**
      * Updates the filter of the filtered person list to filter by the given {@code predicate}.
      * @throws NullPointerException if {@code predicate} is null.
