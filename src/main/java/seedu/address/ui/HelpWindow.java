42b9136588adb48784f6b7e076ed28cc2b82cf7b
henghengyh <yihenglee6@gmail.com>
2026-03-28 15:42:40 +0800

    Remove offline html and edit HelpWindow.java

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -2,11 +2,9 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import java.awt.Desktop;
+import java.net.URI;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.ClipboardContent;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
 
@@ -21,12 +19,6 @@ public class HelpWindow extends UiPart<Stage> {
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
 
-    @FXML
-    private Button copyButton;
-
-    @FXML
-    private Label helpMessage;
-
     /**
      * Creates a new HelpWindow.
      *
@@ -34,7 +26,6 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow(Stage root) {
         super(FXML, root);
-        helpMessage.setText(HELP_MESSAGE);
     }
 
     /**
@@ -44,24 +35,6 @@ public class HelpWindow extends UiPart<Stage> {
         this(new Stage());
     }
 
-    /**
-     * Shows the help window.
-     * @throws IllegalStateException
-     *     <ul>
-     *         <li>
-     *             if this method is called on a thread other than the JavaFX Application Thread.
-     *         </li>
-     *         <li>
-     *             if this method is called during animation or layout processing.
-     *         </li>
-     *         <li>
-     *             if this method is called on the primary stage.
-     *         </li>
-     *         <li>
-     *             if {@code dialogStage} is already showing.
-     *         </li>
-     *     </ul>
-     */
     public void show() {
         logger.fine("Showing help page about the application.");
         getRoot().show();
@@ -89,14 +62,13 @@ public class HelpWindow extends UiPart<Stage> {
         getRoot().requestFocus();
     }
 
-    /**
-     * Copies the URL to the user guide to the clipboard.
-     */
     @FXML
-    private void copyUrl() {
-        final Clipboard clipboard = Clipboard.getSystemClipboard();
-        final ClipboardContent url = new ClipboardContent();
-        url.putString(USERGUIDE_URL);
-        clipboard.setContent(url);
+    private void openUserGuide() {
+        try {
+            Desktop.getDesktop().browse(new URI(USERGUIDE_URL));
+        } catch (Exception e) {
+            logger.warning("Failed to open browser: " + e.getMessage());
+        }
     }
+
 }
