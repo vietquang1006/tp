1f463139d9d996daf0ef8581bf08449b7828f1cc
henghengyh <yihenglee6@gmail.com>
2026-04-02 11:33:53 +0800

    Improve getMessageForPersonsListed

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -16,21 +16,25 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_NO_PERSON_LISTED_OVERVIEW = "No person listed!";
-    public static final String MESSAGE_PERSON_LISTED_OVERVIEW = "%1$d person listed!";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_SINGULAR_LISTED_OVERVIEW = "1 person listed!";
+    public static final String MESSAGE_PLURAL_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
     public static final String MESSAGE_CONTAINS_NON_ALPHANUMERIC_CHARACTER = "Only give alphanumeric keywords";
     public static final String MESSAGE_ONLY_YES_NO = "Please enter either \'y\' or \'n\'";
     public static final String MESSAGE_SUCCESSFUL_CANCEL = "%1$s command cancelled.";
 
+    /**
+     * Returns a message to summarise the number of persons listed.
+     * @param n the number of persons in the list
+     */
     public static String getMessageForPersonsListed(int n) {
         if (n == 0) {
             return MESSAGE_NO_PERSON_LISTED_OVERVIEW;
         } else if (n == 1) {
-            return String.format(MESSAGE_PERSON_LISTED_OVERVIEW, n);
+            return MESSAGE_SINGULAR_LISTED_OVERVIEW;
         } else {
-            return String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, n);
+            return String.format(MESSAGE_PLURAL_LISTED_OVERVIEW, n);
         }
     }
 
